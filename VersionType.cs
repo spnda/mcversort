e13e41b960d6ed6124b7449dfac07c5a11c20709
pnda <43609023+ThePNDA@users.noreply.github.com>
2020-08-06 22:44:25 +0200

    Fix sorting and position offset

--- VersionType.cs
+++ VersionType.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace mcversort {
+﻿namespace mcversort {
     public enum VersionType {
         VANILLA,
         FORGE,
--- mcversort.cs
+++ mcversort.cs
@@ -5,15 +5,14 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text.RegularExpressions;
 
 namespace mcversort {
     class mcversort {
+        static bool checks = false;
+        static bool verbose = false;
+
         static void Main(string[] args) {
             string customPath = null;
-            bool checks = false;
-            bool verbose = false;
-            
             for (int i = 0; i < args.Length; i++) {
                 if (args[i] == "-p") {
                     if (args[i + 1] != null && Directory.Exists(args[i + 1])) {
@@ -32,13 +31,13 @@ namespace mcversort {
                 }
             }
 
-            List<string> versions = Directory.GetDirectories(customPath ?? Util.GetVersionPath()).ToList<string>();
-            if (versions.Count == 0) {
+            string[] versions = Directory.GetDirectories(customPath ?? Util.GetVersionPath());
+            Array.Sort(versions, new NaturalComparer());
+            if (versions.Length == 0) {
                 Console.WriteLine("Warning: No versions found.");
                 return;
             }
             foreach (string version in versions) {
-                Console.WriteLine(version);
                 string jsonLocation = $"{version}/{Path.GetFileName(version)}.json";
                 if (!File.Exists(jsonLocation)) continue;
                 try {
@@ -52,11 +51,11 @@ namespace mcversort {
                         minecraftVersion.InheritingVersion = JsonConvert.DeserializeObject<MinecraftVersion>(File.ReadAllText(inheritingJsonLocation));
                     }
                     if (minecraftVersion.InheritingVersion == null) continue;
+                    Console.WriteLine(minecraftVersion.Id);
                     // Because these third party minecraft versions sometimes use seemingly random release times in their JSON we will need to assign new ones.
                     // We also want future proofing for new versions to come
-                    // Because of this, we will use the version numbers to add to the base release time of the base version.
-                    minecraftVersion.ReleaseTime = minecraftVersion.Time = minecraftVersion.InheritingVersion.ReleaseTime;
-                    //minecraftVersion.ReleaseTime = minecraftVersion.Time = FixVersionTime(minecraftVersion);
+                    // We will get the position in the filesystem alphabetically and then calculate an appropiate offset.
+                    minecraftVersion.ReleaseTime = minecraftVersion.Time = minecraftVersion.InheritingVersion.ReleaseTime.AddMinutes(GetOffsetByPositionInDirectory(versions, minecraftVersion.Id, minecraftVersion.InheritingVersion.Id));
                     File.WriteAllText(jsonLocation, ApplyChangesToJson(minecraftVersion, jsonString));
                 } catch (Exception e) {
                     Console.WriteLine("An error occured. Skipping file.");
@@ -65,40 +64,17 @@ namespace mcversort {
             }
         }
 
-        private static DateTime FixVersionTime(MinecraftVersion minecraftVersion) {
-            DateTime versionStringDateTime = GetDateTimeForVersionString(minecraftVersion);
-            if (versionStringDateTime.Millisecond == 0) return minecraftVersion.ReleaseTime;
-            return Util.AddDateTime(minecraftVersion.InheritingVersion.ReleaseTime, versionStringDateTime);
-        }
-
-        private static DateTime GetDateTimeForVersionString(MinecraftVersion minecraftVersion) {
-            int offset = (int)minecraftVersion.VersionType * 10;
-            switch (minecraftVersion.VersionType) {
-                case VersionType.FORGE: {
-                        // For example 1.16.1-forge-32.0.75
-                        // - 75 will be our new milliseconds
-                        // - 32 + 0 will be our new seconds
-                        // - 16 + 1 will be our new minutes
-                        string[] split = minecraftVersion.Id.Split(".");
-                        return new DateTime(0)
-                            .AddMilliseconds(double.Parse(split.Last()))
-                            .AddSeconds(double.Parse(split[2].Split("-").Last()) + double.Parse(split[split.Length - 2]))
-                            .AddMinutes(double.Parse(split[1]) + double.Parse(split[2].Split("-")[0]));
-                    }
-                case VersionType.OPTIFINE: {
-                        // For example 1.16.1-OptiFine_HD_U_G2_pre10
-                        // - pre10 will be our new milliseconds (Release versions will be 100 milliseconds newer than preview versions
-                        // - G2 (7 + 2) will be our new seconds
-                        // - 16 + 1 will be our new minutes
-                        string[] split = minecraftVersion.Id.Split(".");
-                        string[] opti = split.Last().Split("_");
-                        return new DateTime(0)
-                            .AddMilliseconds(opti.Last().Contains("pre") ? double.Parse(Regex.Replace(opti.Last(), @"\D+", "")) : (double.Parse(opti.Last()) + 100))
-                            .AddSeconds(Util.ConvertStringToInt(opti[3]))
-                            .AddMinutes(double.Parse(split[1]) + double.Parse(split[2].Split("-")[0]));
-                    }
-                default: return new DateTime(0); // If we have a unknown version, don't change it.
+        private static int GetOffsetByPositionInDirectory(string[] versions, string version, string inheritingVersion) {
+            int inheritingPosition = -1, position = -1;
+            for (int i = 0; i < versions.Length; i++) {
+                if (Path.GetFileName(versions[i]) == version) {
+                    position = i;
+                } else if (Path.GetFileName(versions[i]) == inheritingVersion) {
+                    inheritingPosition = i;
+                }
             }
+            if (inheritingPosition == -1 || position == -1) return 0;
+            return position - inheritingPosition;
         }
 
         private static VersionType GetVersionTypeForVersion(MinecraftVersion version) {
@@ -109,6 +85,7 @@ namespace mcversort {
         }
 
         private static string ApplyChangesToJson(MinecraftVersion minecraftVersion, string jsonString) {
+            if (verbose) Console.WriteLine("Applying changes to output JSON for writing...");
             dynamic json = JsonConvert.DeserializeObject(jsonString);
             json["time"] = json["releaseTime"] = minecraftVersion.Time;
             return JsonConvert.SerializeObject(json);
